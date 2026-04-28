f5f0941b38c3ed55b3628d55db121d6f1e4ec2f4
Jerin <jerinji2016@gmail.com>
2026-04-28 21:39:30 +0400

    feat: configure deep linking schemes and update permissions for Android and iOS

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,21 +20,23 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.halooid.splix.mobile"
-        manifestPlaceholders["appAuthRedirectScheme"] = "com.halooid.splix.mobile"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // AppAuth redirect scheme
+        manifestPlaceholders.putAll(
+            mapOf(
+                "appAuthRedirectScheme" to "com.halooid.splix.mobile"
+            )
+        )
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
